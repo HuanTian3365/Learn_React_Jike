7cbd55373257bd42f835245f85160f28a1f6f60c
HuanTian <HuanTian3365543554@outlook.com>
2026-06-08 19:39:51 +0800

    feat: redux接入

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,11 @@ import "./index.scss";
 
 import { RouterProvider } from "react-router-dom";
 import router from "./router/index.tsx";
+import { Provider } from "react-redux";
+import store from "./store";
 
 createRoot(document.getElementById("root")!).render(
-  <RouterProvider router={router} />,
+  <Provider store={store}>
+    <RouterProvider router={router} />
+  </Provider>,
 );
