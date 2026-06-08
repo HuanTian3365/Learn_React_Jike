b3fd12064c3a37fabb4d4ff9a8f671c7fa1b83aa
HuanTian <HuanTian3365543554@outlook.com>
2026-06-08 19:25:05 +0800

    feat: 登录界面

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,7 +1,39 @@
-export default function Login() { 
+import "./index.scss";
+import { Card, Form, Input, Button } from "antd";
+import logo from "@/assets/logo.png";
+
+export default function Login() {
+  function onFinish(values: any) {
+    console.log(values);
+  }
   return (
-    <div>
-      <h1>Ciallo Login</h1>
+    <div className="login">
+      <Card className="login-container">
+        <img className="login-logo" src={logo} alt="" />
+        {/* 登录表单 */}
+        <Form validateTrigger="onBlur" onFinish={onFinish}>
+          <Form.Item
+            name="mobile"
+            rules={[
+              { required: true, message: "请输入手机号" },
+              { pattern: /^1[3-9]\d{9}$/, message: "手机号格式不正确" },
+            ]}
+          >
+            <Input size="large" placeholder="请输入手机号" />
+          </Form.Item>
+          <Form.Item
+            name="code"
+            rules={[{ required: true, message: "请输入验证码" }]}
+          >
+            <Input size="large" placeholder="请输入验证码" />
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit" size="large" block>
+              登录
+            </Button>
+          </Form.Item>
+        </Form>
+      </Card>
     </div>
   );
 }
