7cc2ee7f89ab77196196efae6350013700d2e329
HuanTian <HuanTian3365543554@outlook.com>
2026-06-08 21:03:35 +0800

    feat: Echarts初始化

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,40 @@
-export default function Home() { 
+/* eslint-disable @typescript-eslint/no-unused-expressions */
+import * as echarts from "echarts";
+import { useEffect, useRef } from "react";
+
+export default function Home() {
+  // 1.获取dom节点
+  const chatRef = useRef(null);
+  useEffect(() => {
+    // 2.初始化echarts实例
+    const myChart = echarts.init(chatRef.current);
+
+    // 3.准备图表参数
+    const option = {
+      xAxis: {
+        type: "category",
+        data: ["Vue", "React", "Angular"],
+      },
+      yAxis: {
+        type: "value",
+      },
+      series: [
+        {
+          data: [10, 40, 70],
+          type: "bar",
+        },
+      ],
+    };
+    // 4.设置图表数据
+    option && myChart.setOption(option);
+  }, []);
   return (
     <div>
-      <h1>Ciallo Home</h1>
+      <div
+        ref={chatRef}
+        id="main"
+        style={{ width: "500px", height: "500px" }}
+      ></div>
     </div>
   );
 }
