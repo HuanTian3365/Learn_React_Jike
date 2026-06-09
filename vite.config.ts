c75402029caa0dd7474d76d54ffbaf41663c4386
HuanTian <HuanTian3365543554@outlook.com>
2026-06-09 20:12:29 +0800

    feat: CND优化

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { fileURLToPath } from "url";
 import { visualizer } from "rollup-plugin-visualizer";
+import cdnImport from "vite-plugin-cdn-import";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -23,6 +24,22 @@ export default defineConfig({
     },
   },
   plugins: [
+    cdnImport({
+      modules: [
+        {
+          name: "react",
+          var: "React",
+          path: "umd/react.production.min.js",
+          version: "19.2.6",
+        },
+        {
+          name: "react-dom",
+          var: "ReactDOM",
+          path: "umd/react-dom.production.min.js",
+          version: "19.2.6",
+        },
+      ],
+    }),
     react(),
     visualizer({
       open: true,
